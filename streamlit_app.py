20d4a92809260fced199bc24a186776807f63b2d
sandeeprajrapelly <123927457+sandeeprajrapelly@users.noreply.github.com>
2023-01-30 12:45:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+steamlight.header('Breakfast Menu')
+steam.text('omega 3 & Blueberry Oatmeal')
+steam.text('Kale, Spinsh & Rocket Smoothie')
+steam.text('Hard-Boiled Free - Range Egg')
